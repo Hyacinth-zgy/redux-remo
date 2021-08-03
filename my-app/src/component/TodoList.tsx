d4b15487c976d6a50ad4e326b9e6697ac7040512
@zhaogy <m17681891251@163.com>
2021-08-03 15:39:44 +0800

    配置sass

--- my-app/src/component/TodoList.tsx
+++ my-app/src/component/TodoList.tsx
@@ -1,12 +1,10 @@
 // 类库资源
 import { Input } from 'antd';
-import styled from './TodoList.module.css';
+import styled from '../assets/TodoList.module.scss';
+console.log(styled)
 // 函数组件
 export default function TodoList() {
   return <div className={styled.todolist} >
-    <div>
-
-    </div>
     TODOLIST
   </div>
 }
