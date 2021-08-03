de56f082077debb11ee9481b2db992e2b0326833
@zhaogy <m17681891251@163.com>
2021-08-03 16:08:22 +0800

    简单的使用antd布局

--- my-app/src/component/TodoList.tsx
+++ my-app/src/component/TodoList.tsx
@@ -1,10 +1,29 @@
 // 类库资源
-import { Input } from 'antd';
+import { Input, Button, List } from 'antd';
+import Item from 'antd/lib/list/Item';
 import styled from '../assets/TodoList.module.scss';
+const data = [
+  '在8点开晨会，分配今天的任务代码，',
+  '在9点开晨会，分配今天的任务代码，',
+  '在10点开晨会，分配今天的任务代码，',
+]
 console.log(styled)
 // 函数组件
 export default function TodoList() {
   return <div className={styled.todolist} >
-    TODOLIST
+    <div className={styled.data}>
+      <div className={styled['input-wrap']}>
+        <Input placeholder='Write Something' className={styled.input} />
+        <Button type="primary" className={styled['add-btn']}>ADD</Button>
+      </div>
+      <div className={styled['data-wrap']}>
+        <List bordered dataSource={data} renderItem={
+          item => {
+            return <List.Item>{item}</List.Item>
+          }
+        }>
+        </List>
+      </div>
+    </div>
   </div>
 }
